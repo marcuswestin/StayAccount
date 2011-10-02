847ab61dadd8242d3c65fa124e7177fe099c5b02
Marcus Westin <marcus.westin@gmail.com>
2011-10-02 13:43:25 -0700

    Add schema

---
+++ schema.sql
@@ -0,0 +1,48 @@
+-- DROP database test
+-- CREATE database test
+-- USE test
+
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT, -- `id` int unsigned PRIMARY KEY,
+	-- `email` varchar(256) UNIQUE NOT NULL, -- CHARACTER SET latin1,
+	`device_push_id` varchar(256) DEFAULT NULL
+); -- DEFAULT CHARSET=utf8
+
+CREATE TABLE `intervals` (
+	`id` integer PRIMARY KEY AUTOINCREMENT -- `id` bigint unsigned PRIMARY KEY,
+	`user_id` integer NOT NULL -- int unsigned
+	-- `device_type_id` mediumint unsigned NOT NULL,
+	-- `activity_type_id` int unsigned NOT NULL,
+	-- `start_time` timestamp NOT NULL,
+	-- `duration` int NOT NULL,
+	-- FOREIGN KEY (user_id) REFERENCES users(id),
+	-- FOREIGN KEY (activity_id) REFERENCES activity_types(id),
+	-- FOREIGN KEY (device_type_id) REFERENCES device_types(id)
+);
+
+CREATE TABLE `localities` (
+	`id` integer PRIMARY KEY AUTOINCREMENT, -- `id` bigint unsigned PRIMARY KEY,
+	`user_id` int unsigned NOT NULL,
+	`device_type_id` mediumint unsigned NOT NULL,
+	`time` timestamp NOT NULL,
+	`lat` decimal(10,7) NOT NULL,
+	`lng` decimal(10,7) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (device_type_id) REFERENCES devices(id)
+);
+
+CREATE TABLE `activity_types` (
+	`id` integer PRIMARY KEY AUTOINCREMENT, -- `id` int unsigned PRIMARY KEY,
+	`name` varchar(256) NOT NULL -- CHARACTER SET latin1,
+);
+
+CREATE TABLE `device_types` (
+	`id` integer PRIMARY KEY AUTOINCREMENT, -- `id` mediumint UNSIGNED PRIMARY KEY,
+	`make` varchar(256) NOT NULL,
+	`operating_system` varchar(256) NOT NULL
+);
+
+CREATE TABLE `schema_versions` (
+	`id` integer PRIMARY KEY AUTOINCREMENT,
+	`version` integer
+);
