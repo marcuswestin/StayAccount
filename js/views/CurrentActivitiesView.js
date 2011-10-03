34f9cb5f5288c7f811fa81ed76bc55be83bfc900
Marcus Westin <marcus.westin@gmail.com>
2011-10-03 00:02:58 -0700

    Add a done button - it doesn't do anything yet

--- js/views/CurrentActivitiesView.js
+++ js/views/CurrentActivitiesView.js
@@ -18,7 +18,7 @@ module.exports = Class(View, function() {
 	this.renderHead = function() {
 		this._head.style({ width:'100%', height:headHeight, position:'fixed', top:0, left:0, background:'#abf', textAlign:'center', boxShadow:'#333 0px 1px 2px' }).append(
 			DIV('current activities').style({ textShadow:'#fff 0 1px 0', fontSize:22, marginTop:3 }),
-			DIV('+', { touchstart:bind(this, this._createActivity) }).style({ position:'absolute', top:8, left:8, width:20, height:20, border:'1px outset #fff', borderRadius:4 })
+			DIV('+', { touchstart:bind(this, this._startNewActivity) }).style({ position:'absolute', top:8, left:8, width:20, height:20, border:'1px outset #fff', borderRadius:4 })
 		)
 	}
 	
@@ -39,7 +39,8 @@ module.exports = Class(View, function() {
 			this._currentActivities.empty().append(map(res.results, this, function(currentActivity) {
 				var dateNode = DIV().style({ fontSize:16, color:'#555' }),
 					nameNode = DIV(currentActivity.name).style({ textShadow:'#ccc 0 2px 0' }),
-					node = DIV(nameNode, dateNode).style({ fontSize:20, padding:5, borderBottom:'1px solid #555' })
+					doneButton = DIV('Done', { click:bind(this, this._markCurrentActivityDone, currentActivity.id) }).style({ 'float':'right', fontSize:12, border:'1px outset #ccc', borderRadius:5, padding:'1px 5px', margin:'2px 0' }),
+					node = DIV(doneButton, nameNode, dateNode).style({ fontSize:20, padding:5, borderBottom:'1px solid #555' })
 				setTimeout(function() {
 					time.ago(currentActivity.start_time, function(timeAgo) {
 						dateNode.text('started ' + timeAgo)
@@ -50,13 +51,13 @@ module.exports = Class(View, function() {
 		}))
 	}
 	
-	this._createActivity = function() {
+	this._startNewActivity = function() {
 		if (!this._activityMenuEl) { this._activityMenuEl = DIV().style({ width:300, background:'red' }) }
 		this._activityMenuEl.appendTo(this._head)
 		BT.sql.query('SELECT * FROM activity_types', bind(this, function(err, res) {
 			if (err) { return this._activityMenuEl.text('Error: ' + err) }
 			this._activityMenuEl.append(map(res.results, this, function(result) {
-				return DIV(result.name, { touchstart:bind(this, this._addCurrentActivityWithType, result.id )})
+				return DIV(result.name, { touchstart:bind(this, this._addCurrentActivityWithType, result.id)})
 			}))
 		}))
 	}
@@ -69,9 +70,7 @@ module.exports = Class(View, function() {
 		}))
 	}
 	
-	
-	this._renderActivity = function(result) {
-		return DIV(JSON.stringify(result))
+	this._markCurrentActivityDone = function(currentActivityID) {
+		
 	}
-	
 })
