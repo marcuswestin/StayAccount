604fb5453d2b9a364b44f83031aa326f35779f9b
Marcus Westin <marcus.westin@gmail.com>
2011-10-03 23:19:02 -0700

    script to push all resources to git

---
+++ scripts/git-push.sh
@@ -0,0 +1,7 @@
+cd dependencies/BlowTorch/dependencies/
+cd dom.js; pwd; git push origin master; cd ..;
+cd std.js; pwd; git push origin master; cd ..;
+cd ui.js; pwd; git push origin master; cd ..;
+cd WebViewJavascriptBridge; pwd; git push origin master; cd ..;
+cd ..; pwd; git push origin master;
+cd ../..; pwd; git push origin master;
