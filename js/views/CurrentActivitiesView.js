e4c24103f2adf6943a58447bc0adea446964164a
Marcus Westin <marcus.westin@gmail.com>
2011-10-03 22:35:06 -0700

    Two views: current and completed activities. Enable toggling between them

--- js/views/CurrentActivitiesView.js
+++ js/views/CurrentActivitiesView.js
@@ -1,5 +1,7 @@
 var View = require('./View'),
-	time = require('std/time')
+	time = require('std/time'),
+	extend = require('std/extend'),
+	round = require('std/math/round')
 
 // function bindNodeToQuery(node, query, renderFn) {
 // 	return function() {
@@ -14,48 +16,41 @@ var View = require('./View'),
 module.exports = Class(View, function() {
 	
 	var headHeight = 40
-	this._activitiesTable = 'current_activities'
+	this._activitiesTable = 'completed_intervals'
 	
+	/* Sections
+	 **********/
 	this.renderHead = function() {
 		this._head.style({ width:'100%', height:headHeight, position:'fixed', top:0, left:0, background:'#abf', textAlign:'center', boxShadow:'#333 0px 1px 2px' }).append(
-			DIV('current activities').style({ textShadow:'#fff 0 1px 0', fontSize:22, marginTop:3 }),
-			DIV('+', { click:bind(this, this._startNewActivity) }).style({ position:'absolute', top:8, left:8, width:20, height:20, border:'1px outset #fff', borderRadius:4 }),
-			DIV('I', { touchstart:bind(this, this._setView, 'completed_intervals') }).style({ position:'absolute', top:8, right:20, width:20, height:20, border:'1px outset #fff', borderRadius:4 }),
-			DIV('A', { touchstart:bind(this, this._setView, 'current_activities') }).style({ position:'absolute', top:8, right:45, width:20, height:20, border:'1px outset #fff', borderRadius:4 })
+			this._titleNode = DIV('current activities').style({ textShadow:'#fff 0 1px 0', fontSize:22, marginTop:3 }),
+			DIV('+', { click:bind(this, this._startNewActivity) }).style({ position:'absolute', top:8, left:8, width:30, height:20, border:'1px outset #fff', borderRadius:4 }),
+			DIV('>>', { touchstart:bind(this, this._nextView) }).style({ position:'absolute', top:8, right:8, width:35, height:20, border:'1px outset #fff', borderRadius:4 })
 		)
 	}
 	
 	this.renderBody = function() {
 		this._body.style({ width:305, margin:headHeight+'px auto', background:'#fff', height:'100%', boxShadow:'#fff 0 0 20px' }).append(
-			this._currentActivities = DIV()
+			this._activitiesEl = DIV()
 		)
+		this._nextView()
+	}
+	
+	this._nextView = function() {
+		this._activitiesTable = (this._activitiesTable == 'current_activities' ? 'completed_intervals' : 'current_activities')
+		this._titleNode.text(this._activitiesTable == 'current_activities' ? 'current activities' : 'completed activities')
 		this._refreshCurrentActivities()
 	}
-	
-	this.renderFoot = function() {
-		
-	}
-	
-	this._setView = function(table) {
-		this._activitiesTable = table
-		this._refreshCurrentActivities()
-	}
-	
+
+	/* Querying data and displaying views
+	 ************************************/
 	this._refreshCurrentActivities = function() {
-		BT.sql.query('SELECT ca.id ca_id, ca.start_time ca_start_time, at.name at_name, at.id at_id FROM '+this._activitiesTable+' ca INNER JOIN activity_types at ON ca.activity_type_id = at.id', bind(this, function(err, res) {
-			if (err) { return this._currentActivities.text("Error: " + err) }
-			this._currentActivities.empty().append(map(res.results, this, function(result) {
-				var dateNode = DIV().style({ fontSize:16, color:'#555' }),
-					nameNode = DIV(result.at_name).style({ textShadow:'#ccc 0 2px 0' }),
-					doneButton = DIV('Done', { click:bind(this, this._markCurrentActivityDone, result.ca_id, result.ca_start_time, result.at_id) }).style({ 'float':'right', fontSize:12, border:'1px outset #ccc', borderRadius:5, padding:'1px 5px', margin:'2px 0' }),
-					node = DIV(doneButton, nameNode, dateNode).style({ fontSize:20, padding:5, borderBottom:'1px solid #555' })
-				setTimeout(function() {
-					time.ago(result.ca_start_time, function(timeAgo) {
-						dateNode.text('started ' + timeAgo)
-					})
-				})
-				return node
-			}))
+		var table = this._activitiesTable,
+			selections = ['ca.id', 'ca.start_time', 'at.name', 'at.id']
+		if (table == 'completed_intervals') { selections.push('ca.duration') }
+		var selectionsSql = map(selections, function(selection) { return selection + ' ' + selection.replace('.', '_') }) // ca.start_time becomes ca_start_time
+		BT.sql.query('SELECT '+selectionsSql.join(', ')+' FROM '+table+' ca INNER JOIN activity_types at ON ca.activity_type_id = at.id', bind(this, function(err, res) {
+			if (err) { return this._activitiesEl.text("Error: " + err) }
+			this._activitiesEl.empty().append(map(res.results, this, table == 'completed_intervals' ? this._renderCompletedActivity : this._renderCurrentActivity))
 		}))
 	}
 	
@@ -100,5 +95,46 @@ module.exports = Class(View, function() {
 			if (err) { return alert("Could not complete activity: " + err) }
 			this._refreshCurrentActivities()
 		}))
+	}
+	
+	/* Rendering data
+	 ****************/
+	var rowStyle = { fontSize:20, padding:5, borderBottom:'1px solid #555' },
+		completedRowStyle = extend(rowStyle, { borderBottomColor:'#888' }),
+		titleStyle = { textShadow:'#ccc 0 2px 0' },
+		timeStyle = { fontSize:16, color:'#555' },
+		doneButtonStyle = { 'float':'right', fontSize:12, border:'1px outset #ccc', borderRadius:5, padding:'1px 5px', margin:'2px 0' }
+
+	this._renderCurrentActivity = function(data) {
+		return DIV({ style:rowStyle },
+			DIV('Done', { click:bind(this, this._markCurrentActivityDone, data.ca_id, data.ca_start_time, data.at_id) }).style(doneButtonStyle),
+			DIV(data.at_name).style(titleStyle),
+			DIV({ style:timeStyle },
+				'started ', timeAgoNode(data.ca_start_time)
+			)
+		)
+	}
+
+	this._renderCompletedActivity = function(data) {
+		return DIV({ style:completedRowStyle },
+			DIV(data.at_name).style(titleStyle),
+			DIV({ style:timeStyle },
+				DIV('lasted '+durationString(data.ca_duration)),
+				DIV('started ', timeAgoNode(data.ca_start_time))
+			)
+		)
+	}
+
+	function timeAgoNode(startTime) {
+		var node = SPAN()
+		time.ago(startTime, function(timeAgo) { node.text(timeAgo) })
+		return node
+	}
+
+	function durationString(duration) {
+		if (duration < time.minute) { return 'less than a minute' }
+		if (duration < time.hour) { return Math.floor(duration / time.minute) + ' minutes' }
+		if (duration < time.day) { return Math.floor(duration / time.hour) + ' hours & ' + Math.floor((duration % time.hour) / time.minute) + ' minutes' }
+		return Math.floor(duration / time.day) + ' days & ' + Math.floor((duration % time.day) / time.hour) + ' hours'
 	}
 })
